312c52ef6e9a472cbefeaf5f334d9c43ce5c5c4c
fernestadian888 <78525541+fernestadian888@users.noreply.github.com>
2021-02-19 00:16:32 +0800

    Add files via upload

---
+++ Mahasiswa.php
@@ -0,0 +1,22 @@
+<?php
+
+class Mahasiswa
+{
+
+	public $nim;
+	public $nama;
+	public $tanggal_lahir
+	public $jenis_kelamin
+
+	public funtion tampilkanangkatan()
+    (
+        echo " Nim %this->nim Nama $this->nama Tanggal lahir $this->tanggal_lahir Jenis jenis kelamin
+        $this->jenis_kelamin ";
+    )
+
+    public funtion tampilkanumur()
+    {
+
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Mahasiswa {
+    public funtion nama (fernando) {
+
+    }
+
+    public funtion nim () {
+
+    }
+
+    public funtion tanggal_lahir () {
+ 
+    }
+
+    public funtion jenis_kelamin () {
+
+    }
+}
+
+?>
